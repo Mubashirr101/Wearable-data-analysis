e5c1bd39f066e022ed09b9b59cbebf7cdf065e87
Mubashir <zerex68@gmail.com>
2025-07-29 08:20:17 +0530

    Added python scripts for dataset exploration and filtering functionalities with JSON output for features and filenames

---
+++ backend/services/datasetExploration.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import os
+import re
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+# Exploring Datasets (CSVs) and Understanding their Features
+dataset_path = os.getenv("DATA_PATH")
+
+# Listing all the Features in the CSVs 
+# First removing timestamps from the filenames of CSV files
+def remove_date_suffix(filename):
+    return re.sub(r'\.\d{14}(?=\.csv)','',filename)
+
+# Func to extract and save features of datasets (CSV files) in dict
+def extract_csv_features(folderpath):
+    features_summary={}
+
+    for file in sorted(os.listdir(folderpath)):
+        if file.endswith(".csv"):
+            try:
+                # removing timestamp
+                cleaned_name = remove_date_suffix(file).replace('.csv','')
+                
+                with open(os.path.join(folderpath,file), 'r', encoding='utf-8') as f:
+                     f.readline() # skipping metadata
+                     header_line = f.readline().strip()
+                     headers = header_line.split(',')
+
+                features_summary[cleaned_name] = headers
+            except Exception as e:
+                print(f" Error reading {file}: {e}")
+            
+    return {"summary":features_summary}
+                
+# store the features of each csv file in a json file
+def save_feat(dataset_path):
+    summary_data = extract_csv_features(dataset_path)
+
+    #saving features dict to json file
+    with open("features.json","w") as f:
+        json.dump(summary_data,f,indent=4)
+
+    print("Saved: features.json")
+
+
+# Converting CSV into df
+# lets take 1 csv to convert
+csv_path = r"D:\Projects\Project_7\FitnessTracker\data\raw\Samsung Health\samsunghealth_shaikhmubashir197_20250723145868\com.samsung.health.caffeine_intake.20250723145868.csv"
+
+def cleanNconvert_CSV(path):
+    from io import StringIO
+    cleaned_lines = []
+    with open(path, 'r', encoding='utf-8') as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                continue  # Skip metadata
+            line = line.rstrip('\n').rstrip(',')  # Clean trailing comma
+            cleaned_lines.append(line)
+    return pd.read_csv(StringIO('\n'.join(cleaned_lines)))
+
+# df = cleanNconvert_CSV(csv_path)
+# df.head()
+
---
+++ backend/services/datasetFiltering.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import os
+import re
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+# Filtering and Understanding how the 
+# data files are stored and updated in the SH app export
+
+
+# Saving the list of file names from individual backups
+# backup folders path
+path = os.getenv("BACKUP_PATH")
+
+
+# Cleaning: Removing dates/timestamp from filenames for normalization and ease of comparing
+def remove_timestamp(filename):
+    # This regex matches a dot, 14 digits, then .csv at the end
+    return re.sub(r'\.\d{14}\.csv$','.csv',filename)
+
+# normalizing file names (making sure all the same files across the backups have same names)
+def normalise_filenames(non_normalised_files):
+    norm_backup_csv = {}
+    for backups in non_normalised_files:
+        filelist = []
+        for csv in non_normalised_files[backups]:
+            filelist.append(remove_timestamp(csv))        
+        norm_backup_csv[backups] = filelist
+    # Listing total files in norm_backup_csv (norm), it matches the no. of files in backup_files (non norm)
+    print("\n============= NORMALISED: No. of CSV files in each Backup: =============\n")
+    for i in norm_backup_csv:
+        print(f"\t{i}:{len(norm_backup_csv[i])}")
+    return norm_backup_csv
+
+# saving names of all the backups and the respective csv files in them in a json file
+def savebackups(main_folder_path):
+    backup_files = {}
+    for backups in os.listdir(main_folder_path):
+        backups_path = os.path.join(main_folder_path, backups)
+        if os.path.isdir(backups_path):
+            filelist =[]
+            for file in os.listdir(backups_path):
+                if file.lower().endswith('.csv'):
+                    filelist.append(file)
+            backup_files[backups] = filelist
+    # Checking number of files in each backup
+    print("\n============= RAW: Number of CSV files in each Backup: =============\n")
+    for i in backup_files:
+        print(f"\t{i} : {len(backup_files[i])}")
+
+    # normalizing file names (removing timestamps) across the different backup versions
+    normalisedFilelist = normalise_filenames(backup_files)
+    # adding filenames in json
+    with open("backend/services/filenames.json","w") as f:
+        json.dump(normalisedFilelist,f,indent=4)
+    print (f"\n SAVED FILE: 'filenames.json' ")
+
+
+# Function to find newly added (UNIQUE) files in normalised file list
+def findnewfiles(normalised_files):
+    seen_files = set()
+    print("Newly Added files:")
+    for backups in sorted(normalised_files):
+        print(f"\n {backups}:")
+        new_files =[]
+        for files in sorted(normalised_files[backups]):
+            if files not in seen_files:
+                new_files.append(files)
+                seen_files.add(files)        
+        for index,files in enumerate(new_files,start=1):
+            print(f"    {index}: {files}")
+        
+
+# run the code for finding new files
+with open("backend/services/filenames.json","r") as f:
+    normed_data = json.load(f)
+    findnewfiles(normed_data)
