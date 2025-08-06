9f6daec933e730101b4d4765097fa7082c234087
Mubashir <zerex68@gmail.com>
2025-08-06 08:20:03 +0530

    some changes

--- run_flow.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM === Automatically run the Python flow script ===
-
-cd /d "D:\Projects\Project_7\FitnessTracker"
-call venv\Scripts\activate.bat
-python database\etlflow.py >> logs.txt 2>&1
-
-pause
