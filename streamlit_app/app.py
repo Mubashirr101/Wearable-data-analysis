a1ea79f9601048204913e0021da2acf2dbcfd2a5
Mubashir <zerex68@gmail.com>
2025-08-26 07:55:39 +0530

    Auto update from ETL flow

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
+import os,json
 import urllib.parse
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 import re
 from datetime import timedelta
 import altair as alt
+from supabase import create_client
 
 class app:
     def __init__(self):
         load_dotenv()
         engine = create_engine(f"postgresql+psycopg2://{os.getenv("user")}:{urllib.parse.quote_plus(os.getenv("password"))}@{os.getenv("host")}:{os.getenv("port")}/{os.getenv("dbname")}")        
         # make tabs
-        tab1,tab2 = st.tabs(['Stress Graph','Heart-Rate Graph'])        
+        tab1,tab2,tab3 = st.tabs(['Stress Graph','Heart-Rate Graph','stress-binning'])        
         with tab1:
             df_stress = self.querySupabase(engine,"start_time","score","time_offset","stress","2025-08-17") 
             stresschart = self.chartTimeData(df_stress,"start_time","score","Time/Date","Stress Level","⚡ Stress Level over Time") 
@@ -23,6 +24,10 @@ class app:
             df_hr = self.querySupabase(engine,"heart_rate_end_time","heart_rate_heart_rate","heart_rate_time_offset","tracker_heart_rate","2025-08-17") 
             hrchart = self.chartTimeData(df_hr,"heart_rate_end_time","heart_rate_heart_rate","Time/Date","Heart-Rate","🫀 Heart-Rate over Time") 
             st.altair_chart(hrchart,use_container_width=True)
+        with tab3:
+            offset_col = df_stress["time_offset"]
+            df = self.chartBinningjsons(offset_col)
+            st.dataframe(df)
                 
     def querySupabase(self,engine,xvar,yvar,offset,table,datepattern):
         query = text(f"SELECT {xvar},{yvar},{offset} FROM {table} WHERE {xvar}:: text LIKE :date_pattern")
@@ -45,11 +50,6 @@ class app:
         )
         return chart
 
-
-
-
-
-
     def apply_offset(self,row,offset_col,time_col):
         ## extract offset from the offset feature
         match = re.match(r"UTC([+-])(\d{2})(\d{2})",row[offset_col])
@@ -63,6 +63,22 @@ class app:
             return row[time_col]+delta
         return row[time_col]
 
+    def chartBinningjsons(self,offset_col):
+        url = os.getenv("url")
+        key = os.getenv("key")
+        supabase = create_client(url,key)
+        bucket_name = "json-bucket"
+        file_path = "com.samsung.shealth.stress/b/befaf1f5-fbd9-4f45-a687-2c2c2c5cb2e2.binning_data.json"
+        res = supabase.storage.from_(bucket_name).download(file_path)
+        data = json.loads(res.decode("utf-8"))
+        df = pd.DataFrame(data)
+        df["start_time"] = pd.to_datetime(df["start_time"],unit="ms")
+        df["end_time"] = pd.to_datetime(df["end_time"],unit="ms")
+        df["offset_time"] = offset_col
+        df["start_time"] = df.apply(lambda row: self.apply_offset(row,df["offset_time"],df["start_time"]),axis =1)
+        df["end_time"] = df.apply(lambda row: self.apply_offset(row,df["offset_time"],df["end_time"]),axis =1)
+        df = df.sort_values("start_time")
+        return df
 
 
 if __name__ == "__main__":
