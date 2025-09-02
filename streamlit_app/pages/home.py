967f4c96d0ebe9dffe62b11e5f863be2a78c0bc1
Mubashir <zerex68@gmail.com>
2025-09-02 13:37:52 +0530

    refactor home page layout for improved organization and readability

--- streamlit_app/pages/home.py
+++ streamlit_app/pages/home.py
@@ -35,15 +35,17 @@ def show_home():
             """,
           unsafe_allow_html=True)
     
-    # first row
-    col1,col2,col3,col4,col5 = st.columns([4,2,2,2,2])
+        
+    col1,col2 = st.columns([4,8])        
+         
     with col1:
-        container1 = st.container(border=True)
+        # goal donut
+        goalContainer_1 = st.container(border=True)
         progress_data = pd.DataFrame({
         "Category": ["Completed", "Remaining"],
         "Value": [4, 2]
         })
-        # container1.subheader("🔥Goal Completion")        
+        # goalContainer_1.subheader("🔥Goal Completion")        
         donut_chart = (
         alt.Chart(progress_data,height=100,width=100)
         .mark_arc(innerRadius = 20,outerRadius=40)
@@ -54,36 +56,35 @@ def show_home():
             
             )
         )
-        container1.subheader('⚡Goals Completed')
-        c1c1,c1c2 = container1.columns([1,2])
+        goalContainer_1.subheader('⚡Goals Completed')
+        c1c1,c1c2 = goalContainer_1.columns([1,2])
         c1c1.altair_chart(donut_chart,use_container_width=True)
         c1c2.metric(label='⚡Goals Completed', value='4/6', delta='+1', label_visibility='collapsed')
-    with col2:
-        container2 = st.container(border=True)
-        # container2.markdown("<h3 style = 'text-align:left;'>💤 Sleep</h3>",unsafe_allow_html=True)
-        container2.metric(label='💤 Sleep',value='7.5 hrs',delta='-0.5h')
-    with col3:
-        container3 = st.container(border=True)
-        container3.metric(label='🍎 Calories', value='1800 kcal', delta='+300')
-    with col4:
-        container4 = st.container(border=True)
-        container4.metric(label='👟 Steps',value='8200',delta='+500')
-    with col5:
-        container5 = st.container(border=True)   
-        container5.metric(label='❤️ HR (avg)',value='72 bpm',delta='+5')
 
-
-    # second row 
-    col6,col7 = st.columns([4,8])
-    with col6:
-        container6 = st.container(border=True)
-        container6.subheader('🎯 Your Goals..')
+        ###### goal list         
+        goalContainer_2 = st.container(border=True)
+        goalContainer_2.subheader('🎯 Your Goals..')
         for k , v in goals.items():
-            container6.write(f'- {k}: **{v}**')
-    
-    with col7:
-        container7 = st.container(border=True)
-        container7.subheader('📊Steps Trend (Weekly)')
+            goalContainer_2.write(f'- {k}: **{v}**')
+
+    with col2:        
+        col2_subcol1, col2_subcol2 ,col2_subcol3,col2_subcol4 = st.columns([2,2,2,2])
+        with col2_subcol1:
+            sleepContainer = st.container(border=True)
+            # sleepContainer.markdown("<h3 style = 'text-align:left;'>💤 Sleep</h3>",unsafe_allow_html=True)
+            sleepContainer.metric(label='💤 Sleep',value='7.5 hrs',delta='-0.5h')
+        with col2_subcol2:
+            caloriesContainer = st.container(border=True)
+            caloriesContainer.metric(label='🍎 Calories', value='1800 kcal', delta='+300')
+        with col2_subcol3:
+            stepsContainer = st.container(border=True)
+            stepsContainer.metric(label='👟 Steps',value='8200',delta='+500')
+        with col2_subcol4:
+            hravgContainer = st.container(border=True)   
+            hravgContainer.metric(label='❤️ HR (avg)',value='72 bpm',delta='+5')
+        
+        stepstrendsContainer = st.container(border=True)
+        stepstrendsContainer.subheader('📊Steps Trend (Weekly)')
         steps_chart = (
             alt.Chart(steps_data)
                 .mark_line(point=True)
@@ -94,10 +95,6 @@ def show_home():
                 )
                 .interactive()
         )
-        container7.altair_chart(steps_chart,use_container_width=True)
+        stepstrendsContainer.altair_chart(steps_chart,use_container_width=True)
+       
 
-  
-        
-        
-         
-        
